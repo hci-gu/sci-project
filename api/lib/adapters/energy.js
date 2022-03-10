d077ec71d415131a999ecfa52ecd062a32b0bdaf
Mattias Rost <rrostt@gmail.com>
2022-03-10 16:04:58 +0100

    getEnergy

--- api/lib/adapters/energy.js
+++ api/lib/adapters/energy.js
@@ -1,4 +1,5 @@
 const actilife = require('../actilife')
+const moment = require('moment')
 
 const standardCoeff = {
   constant: -0.022223,
@@ -7,14 +8,39 @@ const standardCoeff = {
   acc: 0.000002,
 }
 
+const group = (data, f) => {
+  const groups = {}
+  data.forEach(d => {
+    const key = f(d)
+    if (!groups[key]) groups[key] = []
+    groups[key].push(d)
+  })
+  return groups
+}
+
 const getEnergy = ({ accel, hr, weight, coeff = standardCoeff }) => {
-  const x = actilife.counts({ acc: accel.map(d => d.x / 9.82), f: 30 })
-  const y = actilife.counts({ acc: accel.map(d => d.y / 9.82), f: 30 })
-  const z = actilife.counts({ acc: accel.map(d => d.z / 9.82), f: 30 })
-  const a = x.map((d, i) => d + y[i] + z[i])
-  
+  const minutes = group(accel, d => moment(d.t).format("YYYY-MM-DD HH:mm"))
+
+  return Object.keys(minutes).map(async minute => {
+    const accel = minutes[minute]
+    const x = await actilife.counts({ acc: accel.map(d => d.x / 9.82), f: 30 })
+    const y = await actilife.counts({ acc: accel.map(d => d.y / 9.82), f: 30 })
+    const z = await actilife.counts({ acc: accel.map(d => d.z / 9.82), f: 30 })
+    const a = x.map((d, i) => d + y[i] + z[i])
+
+    const hrIndex = hr.findIndex(d => moment(d.t).format("YYYY-MM-DD HH:mm") >= minute)
+    if (hrIndex <= 0) return
+    const heartrate = hr[hrIndex].hr
+
+    const energy = coeff.constant + coeff.hr * heartrate + coeff.weight * weight + coeff.acc * a.reduce((a, b) => a + b)
+
+    return {
+      energy,
+      accel,
+    }
+  })
 }
 
 module.exports = {
-
+  getEnergy,
 }
--- api/test/energy.test.js
+++ api/test/energy.test.js
@@ -1,5 +1,10 @@
 const energy = require('../lib/adapters/energy')
+const accel = require('./data/accel.json')
+const hr = require('./data/hr.json')
 
-xdescribe('getEnergy', () => {
-
+describe('getEnergy', () => {
+  test('runs', async () => {
+    const result = await energy.getEnergy({ accel, hr, weight: 80 })
+    expect(result).toHaveLength(991)
+  })
 })
