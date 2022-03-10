b4586c3f523f29880b33a8d4b1721a27c9e1003e
Mattias Rost <rrostt@gmail.com>
2022-03-10 16:23:47 +0100

    Do not use moment.

--- api/lib/adapters/energy.js
+++ api/lib/adapters/energy.js
@@ -1,5 +1,4 @@
 const actilife = require('../actilife')
-const moment = require('moment')
 
 const standardCoeff = {
   constant: -0.022223,
@@ -18,8 +17,13 @@ const group = (data, f) => {
   return groups
 }
 
+const getMinute = ts => {
+  const d = new Date(ts)
+  return `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()} ${d.getHours()}:${d.getMinutes()}`
+}
+
 const getEnergy = ({ accel, hr, weight, coeff = standardCoeff }) => {
-  const minutes = group(accel, d => moment(d.t).format("YYYY-MM-DD HH:mm"))
+  const minutes = group(accel, d => getMinute(d.t))
 
   return Promise.all(Object.keys(minutes).map(async minute => {
     const accel = minutes[minute]
@@ -28,13 +32,13 @@ const getEnergy = ({ accel, hr, weight, coeff = standardCoeff }) => {
     const z = await actilife.counts({ acc: accel.map(d => d.z / 9.82), f: 30 })
     const a = x.map((d, i) => d + y[i] + z[i])
 
-    const hrs = hr.filter(d => moment(d.t).format("YYYY-MM-DD HH:mm") === minute)
+    const hrs = hr.filter(d => getMinute(d.t) === minute)
     const heartrate = hrs.reduce((acc, d) => acc + d.hr, 0) / hrs.length
 
     const energy = coeff.constant + coeff.hr * heartrate + coeff.weight * weight + coeff.acc * a.reduce((a, b) => a + b)
 
     return {
-      minute: moment(minute).toISOString(),
+      minute: (new Date(minute)).toISOString(),
       energy,
       accel,
     }
--- api/test/energy.test.js
+++ api/test/energy.test.js
@@ -2,6 +2,10 @@ const energy = require('../lib/adapters/energy')
 const accel = require('./data/accel.json')
 const hr = require('./data/hr.json')
 
+beforeAll(() => {
+  jest.setTimeout(30000)
+})
+
 describe('getEnergy', () => {
   test('runs', async () => {
     const result = await energy.getEnergy({ accel, hr, weight: 80 })
