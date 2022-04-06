c1be0d90d59878b8e452a86bd40740a7b4b966cd
Mattias Rost <rrostt@gmail.com>
2022-04-06 16:21:33 +0200

    Implements activity route.

---
+++ api/test/routes/users/utils.test.js
@@ -0,0 +1,49 @@
+const db = require('../../../lib/db')
+const utils = require('../../../lib/routes/users/utils')
+
+const models = require('../../../lib/db/models')
+
+// jest.mock('../../../lib/db/models/index.js')
+
+beforeEach(async () => {
+  await db()
+})
+
+describe('activityForPeriod', () => {
+  it('works', async () => {
+    models.User = jest.fn()
+    models.User.mockResolvedValue({})
+    models.AccelCount.find = jest.fn()
+    models.AccelCount.find.mockResolvedValue([
+      {
+        t: '2022-01-01T00:00:00',
+        a: 0,
+        hr: 60,
+      },
+      {
+        t: '2022-01-01T00:01:00',
+        a: 5000,
+        hr: 60,
+      },
+      {
+        t: '2022-01-01T00:02:00',
+        a: 0,
+        hr: 60,
+      },
+      {
+        t: '2022-01-01T00:03:00',
+        a: 0,
+        hr: 60,
+      },
+      {
+        t: '2022-01-01T00:04:00',
+        a: 0,
+        hr: 60,
+      },
+    ])
+
+    const result = await utils.activityForPeriod({ id: -1, from: '2022-01-01', to: '2022-01-02' })
+    expect(result.minutesInactive).toBe(3)
+    expect(result.averageInactiveDuration).toBe(2)
+  })
+})
