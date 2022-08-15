6c54220a1ac2a53b63192c6aeeb3ea4acc647843
Sebastian Andreasson <sebastianandreasson@MacBook-Pro.local>
2022-08-15 15:29:32 +0200

    updated python for running on openshift

--- api/python/app/main.py
+++ api/python/app/main.py
@@ -19,4 +19,4 @@ def calc_counts():
     return jsonify(cs.tolist())
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=80)
+    app.run(host="0.0.0.0", debug=True, port=8080)
