144d5af7089f717c60f18fcdced10819480ccc74
Mattias Rost <rrostt@gmail.com>
2023-02-06 16:19:51 +0100

    Calcs counts in javascript.

--- acc2counts/counts.js
+++ acc2counts/counts.js
@@ -85,16 +85,21 @@ const getCounts = (values) => {
   return runsum(truncated, integN, 0)
 }
 
-export default (acc) => {
-  const [xs, ys, zs] = [
-    getCounts(acc.filter((_, i) => i % 3 === 0).map((d) => d / 9.82)),
-    getCounts(acc.filter((_, i) => i % 3 === 1).map((d) => d / 9.82)),
-    getCounts(acc.filter((_, i) => i % 3 === 2).map((d) => d / 9.82)),
-  ]
-  const x = xs.reduce((a, b) => a + b)
-  const y = ys.reduce((a, b) => a + b)
-  const z = zs.reduce((a, b) => a + b)
-  const accVM = Math.sqrt(x * x + y * y + z * z)
+// export default (acc) => {
+//   const [xs, ys, zs] = [
+//     getCounts(acc.filter((_, i) => i % 3 === 0).map((d) => d / 9.82)),
+//     getCounts(acc.filter((_, i) => i % 3 === 1).map((d) => d / 9.82)),
+//     getCounts(acc.filter((_, i) => i % 3 === 2).map((d) => d / 9.82)),
+//   ]
+//   const x = xs.reduce((a, b) => a + b)
+//   const y = ys.reduce((a, b) => a + b)
+//   const z = zs.reduce((a, b) => a + b)
+//   const accVM = Math.sqrt(x * x + y * y + z * z)
 
-  return accVM
-}
+//   return accVM
+// }
+
+// load data.json
+const data = require('./data.json')
+
+console.log(getCounts(data))
