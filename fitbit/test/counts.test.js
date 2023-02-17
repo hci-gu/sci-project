36bc78705a482f592e367c95b9b7e25a3e42bf9f
Sebastian Andreasson <sebastianandreasson@MacBook-Pro.local>
2023-02-17 09:52:12 +0100

    improve mem usage, refactor to inline

--- fitbit/test/counts.test.js
+++ fitbit/test/counts.test.js
@@ -1,21 +1,25 @@
 import calculateCounts from '../app/counts'
-import calculateCountsSplit from '../app/counts_split'
+// import calculateCounts from '../app/counts_memwatch'
 import accData from './accData'
 
 describe('calculateCounts', () => {
   it('should return the correct counts', async () => {
-    const accX = new Float32Array(1800)
-    const accY = new Float32Array(1800)
-    const accZ = new Float32Array(1800)
+    const length = 1800
+    const accX = new Float32Array(length)
+    const accY = new Float32Array(length)
+    const accZ = new Float32Array(length)
 
     // accData is a 5400 length array of 3 axis values
-    for (let i = 0; i < 1800; i++) {
+    for (let i = 0; i < length; i++) {
       accX[i] = accData[i * 3] / 9.82
       accY[i] = accData[i * 3 + 1] / 9.82
       accZ[i] = accData[i * 3 + 2] / 9.82
     }
 
     const counts = await calculateCounts(accX, accY, accZ)
-    expect(counts).toEqual(15351.626428492846)
+    // expect(counts).toEqual(15351.626428492846) // before memchanges
+    // expect(counts).toEqual(15388.939729559019) // after small bugfix
+    // expect(counts).toEqual(15575.106099157078) // changed to Float32Array AB2
+    expect(counts).toEqual(15569.778579029311) // changed to Float32Array AB
   })
 })
