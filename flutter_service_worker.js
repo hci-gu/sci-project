0f519b0d9a092cd31c0a80f71f772746883ef4e8
sebastianandreasson <sebastian.andreasson93@gmail.com>
2023-09-05 08:31:41 +0000

    Deploying to gh-pages from @ hci-gu/sci-project@e34515f4eb1d391a794d349ef99d41d05536994a 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,56 +2,69 @@
 const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
-const RESOURCES = {
-  "version.json": "afe80f521a09646d1016d2ba1141a6fa",
-"assets/FontManifest.json": "eda3f246271a3a4de7fa1c70eb5fc7a7",
-"assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
-"assets/assets/fonts/Manrope-Regular.ttf": "d132ed5224d61c7c2c71e44cd2750999",
-"assets/assets/fonts/Manrope-ExtraLight.ttf": "fc80ad19afcbea34e8dbdedb9e60bd45",
-"assets/assets/fonts/Manrope-Light.ttf": "d0704eb4a339c1895bf5d7a153a25c84",
-"assets/assets/fonts/Manrope-Bold.ttf": "2af19b388ce4f0e3617fed61faea284e",
-"assets/assets/fonts/Manrope-SemiBold.ttf": "be79203f7047b78f1374f8658fe01208",
-"assets/assets/fonts/Manrope-Medium.ttf": "36bd05140475db525b9617f601c201a6",
-"assets/assets/fonts/Manrope-ExtraBold.ttf": "5167c303a88f05722db3b07c584cbb40",
-"assets/assets/png/ryggmarg_logo.png": "de84e84b5408560623f7ddb99ab1d046",
-"assets/assets/svg/hand.svg": "b5e5831de2ea4932bdae0bc61cddbfbd",
-"assets/assets/svg/empty_state.svg": "dde4739d2edae1639b5914423d9fc3f6",
-"assets/assets/svg/back.svg": "78af4432c32978ccb5ecb0ca18a88f1b",
-"assets/assets/svg/flame.svg": "9965b037e8c0faeff50d77053c14b696",
-"assets/assets/svg/wheelchair.svg": "62f1fb4f6c05a14ef57b6d5b2257506e",
-"assets/assets/svg/exercise.svg": "1c8990a56bf4fa2cee62dca2d7248e84",
-"assets/assets/svg/shoulderJoint.svg": "751c1df53ee547739a38e6aa3bc99760",
-"assets/assets/svg/person.svg": "312ee8ae1cdc3b375ecd477f98fb95af",
-"assets/assets/svg/scapula.svg": "8ea55719af7d97ebfbf8eee3451df3e5",
-"assets/assets/svg/elbow.svg": "2291853d3755d869e900f19d2a003cbd",
-"assets/assets/svg/neck.svg": "8b2e0477b4d9553742a2c426f2d6c08a",
-"assets/assets/licenses/icon_license.txt": "023eedd873ff348626a8757161290164",
-"assets/NOTICES": "56a648166652f33edafaa593b5802f4a",
-"assets/AssetManifest.json": "0295777da66eea1554a2e07d04c90199",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"main.dart.js": "07186ddb79269e63ac2e5346e4ea6ba8",
-"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
-"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
-"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
-"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
-"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e",
-"manifest.json": "60cf0e8a29d91fc890f252903819f5ae",
-"index.html": "27bf4d18d829efc454426d0b7f0516b3",
-"/": "27bf4d18d829efc454426d0b7f0516b3",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea"
-};
 
+const RESOURCES = {"assets/assets/fonts/Manrope-Regular.ttf": "d132ed5224d61c7c2c71e44cd2750999",
+"assets/assets/fonts/Manrope-Light.ttf": "d0704eb4a339c1895bf5d7a153a25c84",
+"assets/assets/fonts/Manrope-SemiBold.ttf": "be79203f7047b78f1374f8658fe01208",
+"assets/assets/fonts/Manrope-Bold.ttf": "2af19b388ce4f0e3617fed61faea284e",
+"assets/assets/fonts/Manrope-ExtraLight.ttf": "fc80ad19afcbea34e8dbdedb9e60bd45",
+"assets/assets/fonts/Manrope-ExtraBold.ttf": "5167c303a88f05722db3b07c584cbb40",
+"assets/assets/fonts/Manrope-Medium.ttf": "36bd05140475db525b9617f601c201a6",
+"assets/assets/images/pressure_release_left.jpeg": "43c3e85e624378380c0be73664c2c8f5",
+"assets/assets/images/pressure_ulcer_map.png": "b9bf41de4ec5649bd4ac3690b4532e8a",
+"assets/assets/images/pressure_release_forward.jpeg": "0bb98a11f57ee85256d8debfbcb771ca",
+"assets/assets/images/ryggmarg_logo.png": "de84e84b5408560623f7ddb99ab1d046",
+"assets/assets/images/pressure_release_lying.jpeg": "b62a778e1344f331df15f463ecfbe7be",
+"assets/assets/images/pressure_release_right.jpeg": "8616fe635ec4663f8fed52c83fb70ccf",
+"assets/assets/images/right.png": "a3c514c328567b033b13e66f1dfacf8b",
+"assets/assets/images/fitbit.png": "d9b04dc58e5bdd0bf7787fd35a14bb9c",
+"assets/assets/licenses/icon_license.txt": "023eedd873ff348626a8757161290164",
+"assets/assets/svg/hand.svg": "b5e5831de2ea4932bdae0bc61cddbfbd",
+"assets/assets/svg/goal_done.svg": "262c608bdb514fee829b031114457991",
+"assets/assets/svg/elbow.svg": "2291853d3755d869e900f19d2a003cbd",
+"assets/assets/svg/back.svg": "78af4432c32978ccb5ecb0ca18a88f1b",
+"assets/assets/svg/set_goal.svg": "b0ce64db60182b2b79655f2f1b160672",
+"assets/assets/svg/wheelchair.svg": "62f1fb4f6c05a14ef57b6d5b2257506e",
+"assets/assets/svg/flame.svg": "9965b037e8c0faeff50d77053c14b696",
+"assets/assets/svg/neck.svg": "8b2e0477b4d9553742a2c426f2d6c08a",
+"assets/assets/svg/person.svg": "312ee8ae1cdc3b375ecd477f98fb95af",
+"assets/assets/svg/empty_state.svg": "dde4739d2edae1639b5914423d9fc3f6",
+"assets/assets/svg/shoulderJoint.svg": "751c1df53ee547739a38e6aa3bc99760",
+"assets/assets/svg/exercise.svg": "1c8990a56bf4fa2cee62dca2d7248e84",
+"assets/assets/svg/alarm.svg": "91bb66291a4c3e002c76716afbb4d50a",
+"assets/assets/svg/scapula.svg": "8ea55719af7d97ebfbf8eee3451df3e5",
+"assets/fonts/MaterialIcons-Regular.otf": "4dada8b9cc3ccdf14d0b50bbd8c8b878",
+"assets/AssetManifest.json": "ec4cbb68eac7925323eb5112ec40d1e2",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
+"assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
+"assets/AssetManifest.bin": "49cd1c37a818ffdb863539f01c1dcc47",
+"assets/FontManifest.json": "eda3f246271a3a4de7fa1c70eb5fc7a7",
+"assets/NOTICES": "1a93b19b0f128901c64e9f02bbf8317e",
+"version.json": "aadc8db379c89faf2567d038a785a279",
+"manifest.json": "60cf0e8a29d91fc890f252903819f5ae",
+"index.html": "d7d45348ee9797887a98c569561e285b",
+"/": "d7d45348ee9797887a98c569561e285b",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"main.dart.js": "710877fe64fda5aefe6ccd38056377da",
+"canvaskit/canvaskit.wasm": "d9f69e0f428f695dc3d66b3a83a4aa8e",
+"canvaskit/skwasm.wasm": "d1fde2560be92c0b07ad9cf9acb10d05",
+"canvaskit/canvaskit.js": "5caccb235fad20e9b72ea6da5a0094e6",
+"canvaskit/skwasm.worker.js": "51253d3321b11ddb8d73fa8aa87d3b15",
+"canvaskit/chromium/canvaskit.wasm": "393ec8fb05d94036734f8104fa550a67",
+"canvaskit/chromium/canvaskit.js": "ffb2bb6484d5689d91f393b60664d530",
+"canvaskit/skwasm.js": "95f16c6690f955a45b2317496983dbe9"};
 // The application shell files that are downloaded before a service worker can
 // start.
-const CORE = [
-  "main.dart.js",
+const CORE = ["main.dart.js",
 "index.html",
 "assets/AssetManifest.json",
 "assets/FontManifest.json"];
+
 // During install, the TEMP cache is populated with the application shell files.
 self.addEventListener("install", (event) => {
   self.skipWaiting();
@@ -62,7 +75,6 @@ self.addEventListener("install", (event) => {
     })
   );
 });
-
 // During activate, the cache is populated with the temp files downloaded in
 // install. If this service worker is upgrading from one with a saved
 // MANIFEST, then use this to retain unchanged resource files.
@@ -84,6 +96,8 @@ self.addEventListener("activate", function(event) {
         await caches.delete(TEMP);
         // Save the manifest to make future upgrades efficient.
         await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
         return;
       }
       var oldManifest = await manifest.json();
@@ -109,6 +123,8 @@ self.addEventListener("activate", function(event) {
       await caches.delete(TEMP);
       // Save the manifest to make future upgrades efficient.
       await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
       return;
     } catch (err) {
       // On an unhandled exception the state of the cache cannot be guaranteed.
@@ -119,7 +135,6 @@ self.addEventListener("activate", function(event) {
     }
   }());
 });
-
 // The fetch handler redirects requests for RESOURCE files to the service
 // worker cache.
 self.addEventListener("fetch", (event) => {
@@ -159,7 +174,6 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
-
 self.addEventListener('message', (event) => {
   // SkipWaiting can be used to immediately activate a waiting service worker.
   // This will also require a page refresh triggered by the main worker.
@@ -172,7 +186,6 @@ self.addEventListener('message', (event) => {
     return;
   }
 });
-
 // Download offline will check the RESOURCES for all files not in the cache
 // and populate them.
 async function downloadOffline() {
@@ -193,7 +206,6 @@ async function downloadOffline() {
   }
   return contentCache.addAll(resources);
 }
-
 // Attempt to download the resource online before falling back to
 // the offline cache.
 function onlineFirst(event) {
